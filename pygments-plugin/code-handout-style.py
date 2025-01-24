5c3fbcb1e88bc720d61965521d6d3a2c6b875a8f
Siphesihle Mhlongo <sihlecodes@gmail.com>
2025-01-24 11:24:47 +0200

    Minor color changes to improve readability
    Remove commented code

--- pygments-plugin/code-handout-style.py
+++ pygments-plugin/code-handout-style.py
@@ -1,31 +1,15 @@
 from pygments.style import Style
 from pygments.token import (Token, Comment, Error, Generic, Name, Number, Operator,
                             String, Text, Whitespace, Keyword)
-# BACKGROUND    = '#120f17'
-# TEXT          = '#cdbccf'
-# WHITESPACE    = '#cdbccf'
-# COMMENT       = '#88788f'
-# FUNCTION      = '#5f4d8c'
-# TYPE          = '#ffc354'
-# PRIMATIVE     = '#ffc354'
-# KEYWORD       = '#ff505d'
-# STRING        = '#b897ff'
-# NUMBER        = '#ff505d'
 
-# UNDEFINED     = '#ff0000'
-# STRING_ESCAPE = UNDEFINED
-# ATTRIBUTE     = UNDEFINED
-# STRING_OTHER  = UNDEFINED
-# TAG           = UNDEFINED
-# OPERATOR      = UNDEFINED
 
 BACKGROUND    = '#fafbfc'
-TEXT          = '#5d6165'
+TEXT          = '#4a5e52'
 WHITESPACE    = '#cdbccf'
-COMMENT       = '#abaeb1'
+COMMENT       = '#9a9da3'
 FUNCTION      = '#e1af52'
 TYPE          = '#669de0'
-PRIMATIVE     = '#e98d48'
+PRIMITIVE     = '#e98d48'
 KEYWORD       = '#e98d48'
 STRING        = '#91b138'
 STRING_ESCAPE = '#77bca8'
@@ -38,24 +22,26 @@ STRING_OTHER  = UNDEFINED
 TAG           = UNDEFINED
 
 
-# hello world
 class HandoutStyle(Style):
     background_color = BACKGROUND
     highlight_color = TEXT
 
     styles = {
+        Text: TEXT,
         Token: TEXT,
+
+        Whitespace: WHITESPACE,
 
         Comment: f'{COMMENT}',
         Comment.Preproc: 'noitalic',
         Comment.Special: 'bold',
 
         Generic: TEXT,
-        Error: f'bg:#ff0000 #ffffff',
+        Error: 'bg:#ff0000 #ffffff',
 
         Keyword: f'{KEYWORD}',
         Keyword.Pseudo: 'nobold',
-        Keyword.Type: PRIMATIVE,
+        Keyword.Type: PRIMITIVE,
 
         Name.Attribute: f'italic {ATTRIBUTE}',
         Name.Builtin: f'{FUNCTION}',
@@ -77,8 +63,4 @@ class HandoutStyle(Style):
         String.Escape: f'{STRING_ESCAPE}',
         String.Other: STRING_OTHER,
         String.Symbol: f'bold {STRING_OTHER}',
-
-        Text: TEXT,
-
-        Whitespace: WHITESPACE,
     }
